acfc433212400c5de6cd4781f6bc785fb20e871b
kristal <Aleks.golovany@gmail.com>
2023-02-07 23:55:27 +0200

    Created site communication configuration class

--- HDrezka/connector.py
+++ HDrezka/connector.py
@@ -1,16 +1,43 @@
 import requests
+from constants import USER_AGENT, DOMAIN
 
 
 class SiteConnector:
-    instance = None
-
+    """
+    SiteConnector предназначен для удобной реализации запросов без необходимости повторной генерации данных запроса
+    """
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "instance"):
-            cls.instance = super(SiteConnector, cls).__new__(cls, *args, **kwargs)
+            cls.instance = super(SiteConnector, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
-        pass
+    def __init__(self, domain=DOMAIN, user_agent=USER_AGENT, proxy: dict = None):
+        """
+        Генерирует стартовые настройки для запросов
 
+        :param domain: домен сайта
+        :param user_agent: можно загрузить своего юзер-агента или оставить стандартного
+        :param proxy: IP адрес прокси, изначально отсутствует
+        """
+        self.domain = domain
+        self.url = f"https://{self.domain}/"
+        self.user_agent = user_agent
+        self.proxy = proxy
 
+        self.session = requests.Session()
+        self.session.headers = self.header()
+        self.session.proxies = self.proxy
 
+    def header(self):
+        """
+        Функция генерирует и возвращает заголовок запроса
+
+        :return: заголовок запроса
+        """
+        return {
+            "Host": self.domain,
+            "Origin": "http://" + self.domain,
+            "Referer": self.url,
+            "User-Agent": self.user_agent,
+            "X-Requested-With": "XMLHttpRequest"
+        }
--- HDrezka/constants.py
+++ HDrezka/constants.py
@@ -4,3 +4,6 @@ SERIAL = "Serial"
 ACTION_FILM = "get_movie"
 ACTION_SEASON = "get_stream"
 ACTION_SUBTITLE = "get_episodes"
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0"
+DOMAIN = "rezka.ag"
