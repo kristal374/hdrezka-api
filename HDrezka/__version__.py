956e928e4a69a992b651ad0d4cd9d33858831e5c
kristal <Aleks.golovany@gmail.com>
2023-03-06 13:38:58 +0200

    Created __version__.py file and added package information.

---
+++ HDrezka/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'HDrezka'
+__version__ = '1.0.0'
+__license__ = 'MIT'
+__author__ = 'Alexandr Golovany'
+__contact__ = 'mail.aleks.for.work@gmail.com'
+__url__ = 'https://github.com/kristal374/hdrezka-api'
+__copyright__ = 'Copyright Alexandr Golovany'
+__description__ = 'An unofficial Hdrezka.ag API that will help you get' \
+                  'all the information you need about a video or movie page.'
