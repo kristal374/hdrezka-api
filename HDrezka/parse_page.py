e5176b320b1a68217bdeabeda0cdd0d391bb5b7d
kristal <Aleks.golovany@gmail.com>
2023-03-16 13:35:28 +0200

    Type annotations have been added or extended, and data validation has also been added.

--- HDrezka/parse_page.py
+++ HDrezka/parse_page.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from requests import exceptions
+from typing import Optional, Union, Dict
 from urllib.parse import quote
 
 from HDrezka.connector import SiteConnector
@@ -16,8 +17,8 @@ class BaseSingleCategory(ABC):
         return super(BaseSingleCategory, cls).__new__(cls)
 
     def __init__(self):
-        self._path = {"genre": '', "page": ''}
-        self._modifier = {"filter": '', "display": ''}
+        self._path: Dict[str, Optional[Union[str, int]]] = {"genre": '', "page": ''}
+        self._modifier: Dict[str, Optional[str]] = {"filter": '', "display": ''}
 
     @abstractmethod
     def get(self):
@@ -28,7 +29,12 @@ class BaseSingleCategory(ABC):
                 f"Failed to establish a new connection. Max retries exceeded with url: {self}") from None
         return response.text
 
-    def page(self, n):
+    def page(self, n: int):
+        if isinstance(n, bool) or not isinstance(n, int) or n <= 0:
+            if isinstance(n, str) and n.isdigit() and n != "0":
+                n = int(n)
+            else:
+                raise AttributeError("Attribute \"n\" must be of type \"int\" and greater than 0")
         self._path["page"] = n
         return self
 
@@ -47,16 +53,24 @@ class BaseSingleCategory(ABC):
 
 class BaseCategory(BaseSingleCategory, ABC):
     @abstractmethod
-    def selected_category(self, genre):
+    def selected_category(self, genre: Optional[str]):
+        if not isinstance(genre, str) and genre is not None:
+            raise AttributeError("Attribute \"genre\" must be of type \"str\". Use ready-made genres filters.Genre*")
         self._path["genre"] = genre
         return self
 
-    def filter(self, pattern: Filters = Filters.LAST):
+    def filter(self, pattern: Optional[Union[Filters, str]] = Filters.LAST):
+        if pattern is None:
+            self._modifier["filter"] = ""
+            return self
+        elif not isinstance(pattern, str):
+            raise AttributeError(
+                "Attribute \"pattern\" must be of type \"str\". Use ready-made genres in filters.Filters")
         self._modifier["filter"] = f"?filter={pattern}"
         return self
 
     @abstractmethod
-    def find_best(self, *, genre=None, year: int = None):
+    def find_best(self, genre: Optional[str] = None, year: int = None):
         select_genre = genre if genre is not None else self._path.get("genre")
         return Best(self._name).select(genre=select_genre, year=year)
 
@@ -64,10 +78,10 @@ class BaseCategory(BaseSingleCategory, ABC):
 class Films(BaseCategory):
     _name = "films"
 
-    def selected_category(self, genre: GenreFilm):
+    def selected_category(self, genre: Optional[Union[GenreFilm, str]]):
         return super(Films, self).selected_category(genre)
 
-    def find_best(self, *, genre: GenreFilm = None, year: int = None):
+    def find_best(self, genre: Optional[Union[GenreFilm, str]] = None, year: Optional[int] = None):
         return super(Films, self).find_best(genre=genre, year=year)
 
     def get(self):
@@ -77,10 +91,10 @@ class Films(BaseCategory):
 class Cartoons(BaseCategory):
     _name = "cartoons"
 
-    def selected_category(self, genre: GenreCartoons):
+    def selected_category(self, genre: Optional[Union[GenreCartoons, str]]):
         return super(Cartoons, self).selected_category(genre)
 
-    def find_best(self, *, genre: GenreCartoons = None, year: int = None):
+    def find_best(self, genre: Optional[Union[GenreCartoons, str]] = None, year: int = None):
         return super(Cartoons, self).find_best(genre=genre, year=year)
 
     def get(self):
@@ -90,10 +104,10 @@ class Cartoons(BaseCategory):
 class Series(BaseCategory):
     _name = "series"
 
-    def selected_category(self, genre: GenreSeries):
+    def selected_category(self, genre: Optional[Union[GenreSeries, str]]):
         return super(Series, self).selected_category(genre)
 
-    def find_best(self, *, genre: GenreSeries = None, year: int = None):
+    def find_best(self, genre: Optional[Union[GenreSeries, str]] = None, year: int = None):
         return super(Series, self).find_best(genre=genre, year=year)
 
     def get(self):
@@ -103,10 +117,10 @@ class Series(BaseCategory):
 class Animation(BaseCategory):
     _name = "animation"
 
-    def selected_category(self, genre: GenreAnimation):
+    def selected_category(self, genre: Optional[Union[GenreAnimation, str]]):
         return super(Animation, self).selected_category(genre)
 
-    def find_best(self, *, genre: GenreAnimation = None, year: int = None):
+    def find_best(self, genre: Optional[Union[GenreAnimation, str]] = None, year: int = None):
         return super(Animation, self).find_best(genre=genre, year=year)
 
     def get(self):
@@ -116,11 +130,20 @@ class Animation(BaseCategory):
 class New(BaseSingleCategory):  # noqa
     _name = "new"
 
-    def filter(self, pattern: Filters = Filters.LAST):
+    def filter(self, pattern: Optional[Union[Filters, str]] = Filters.LAST):
+        if pattern is None:
+            self._modifier["filter"] = ""
+            return self
+        if not isinstance(pattern, str):
+            raise AttributeError(
+                "Attribute \"pattern\" must be of type \"str\". Use ready-made genres in filters.Filters")
         self._modifier["filter"] = f"?filter={pattern}"
         return self
 
-    def show_only(self, pattern: ShowCategory = ShowCategory.ALL):
+    def show_only(self, pattern: Optional[Union[ShowCategory, str, int]] = ShowCategory.ALL):
+        if not (isinstance(pattern, str) or isinstance(pattern, int)):
+            raise AttributeError("Attribute \"pattern\" must be of type \"str\" or \"int\". "
+                                 "Use ready-made pattern in filters.ShowCategory")
         self._modifier["display"] = f"genre={pattern}"
         return self
 
@@ -150,6 +173,11 @@ class Search(BaseSingleCategory):  # noqa
         self._search_text = None
 
     def query(self, text: str):
+        if text is None:
+            self._search_text = None
+            return self
+        elif not isinstance(text, str):
+            raise AttributeError("Attribute \"text\" must be of type \"str\".")
         quote_text = quote(text)
         self._search_text = f"?do=search&subaction=search&q={quote_text}"  # noqa
         return self
@@ -167,7 +195,11 @@ class Best(BaseSingleCategory):  # noqa
         super().__init__()
         self._best_params = [name, "best"]
 
-    def select(self, *, genre=None, year: int = None):
+    def select(self, genre: Optional[str] = None, year: int = None):
+        if year is not None and (isinstance(year, bool) or not isinstance(year, int) or year < 1895):
+            raise AttributeError("Attribute \"year\" must be of type \"int\" and greater than or equal to 1895")
+        elif genre is not None and not isinstance(genre, str):
+            raise AttributeError("Attribute \"genre\" must be of type \"str\". Use ready-made genres in filters.Genre*")
         self._best_params = self._best_params[:2]
         if genre is not None and genre != '':
             self._best_params.append(genre)
