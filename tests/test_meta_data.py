1a1c567bcb8dcabce89a91146492d14d2f81a606
kristal <Aleks.golovany@gmail.com>
2023-08-05 22:12:43 +0300

    Добавлены тесты для проверки метаданных пакета

---
+++ tests/test_meta_data.py
@@ -0,0 +1,21 @@
+from unittest import TestCase, main
+
+import HDrezka
+
+
+class TestMetaData(TestCase):
+    def test_metadata_variables(self):
+        def is_non_empty_string(s):
+            self.assertIsInstance(s, (str, bytes))
+            self.assertEqual(bool(s), True, msg=f"Element is empty")
+        is_non_empty_string(HDrezka.__title__)
+        is_non_empty_string(HDrezka.__version__)
+        is_non_empty_string(HDrezka.__license__)
+        is_non_empty_string(HDrezka.__author__)
+        is_non_empty_string(HDrezka.__contact__)
+        is_non_empty_string(HDrezka.__url__)
+        is_non_empty_string(HDrezka.__description__)
+
+
+if __name__ == '__main__':
+    main()
