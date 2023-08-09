d770e56cdd5e99c88367a893e18accd558335823
kristal <Aleks.golovany@gmail.com>
2023-08-09 14:42:48 +0300

    Добавлены __init__.py и __main__.py для единой точки запуска всех тестов

---
+++ tests/__main__.py
@@ -0,0 +1,13 @@
+from test_film import TestFilms
+from test_cartoon import TestCartoons
+from test_series import TestSeries
+from test_animation import TestAnimations
+from test_announce import TestAnnounce
+from test_new import TestNew
+from test_collections import TestCollections
+from test_search import TestSearch
+from test_meta_data import TestMetaData
+from unittest import main
+
+if __name__ == '__main__':
+    main()
