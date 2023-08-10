e2c7109d1d291d437122addaa42f53a9bd754cfa
kristal <Aleks.golovany@gmail.com>
2023-08-10 15:32:42 +0300

    Реализован тестовый кейс .get() у класса Animation

--- tests/test_animation.py
+++ tests/test_animation.py
@@ -1,9 +1,13 @@
-from random import randint
 import re
-from unittest import TestCase, skip
+import json
+import requests
+import requests_mock
 
-from HDrezka.filters import GenreAnimation, Filters
+from random import randint
+from unittest import TestCase
+
 from HDrezka.parse_page import Animation
+from HDrezka.filters import GenreAnimation, Filters
 
 
 class TestAnimations(TestCase):
@@ -113,6 +117,33 @@ class TestAnimations(TestCase):
             correct_url = f"https://rezka.ag/animation/best/{genre}/{year}/page/{page}/"
             self.assertEqual(correct_url, response)
 
-    @skip
-    def test_get(self):
-        self.fail()
+    @requests_mock.Mocker()
+    def test_positive_get(self, m):
+        with open("mock_html/animation_4.html", encoding="utf-8") as file:
+            text = file.read()
+
+        with open("mock_html/reference_data.json", "r", encoding="utf-8") as json_file:
+            reference_data = json.loads(json_file.read())
+
+        correct_url = "https://rezka.ag/animation/page/4/"
+        m.register_uri('GET', correct_url, text=text)
+        site = self.movie.page(4)
+
+        self.assertEqual(correct_url, site.__str__())
+
+        response = [i.__dict__ for i in site.get()]
+        self.assertListEqual(reference_data["animation"], response)
+
+        site = self.movie.find_best(year=2018)
+        m.register_uri('GET', site.__str__(), text="Success")
+        self.assertEqual(0, len(site.get()))
+
+    @requests_mock.Mocker()
+    def test_negative_get(self, m):
+        correct_url = "https://rezka.ag/animation/page/4/"
+        site = self.movie.page(4)
+        self.assertEqual(correct_url, site.__str__())
+
+        m.register_uri('GET', correct_url, exc=requests.exceptions.ConnectionError)
+        with self.assertRaises(requests.exceptions.ConnectionError):
+            site.get()
