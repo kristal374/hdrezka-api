9ee7b9a0f4fe645efd31c5802d6d5a4ce87a3f55
kristal <Aleks.golovany@gmail.com>
2023-08-14 22:26:55 +0300

    Импорты заменены на абсолютные, импорты перенесены в __init__.py

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,15 +1,5 @@
-import tests
+from tests import *
 
-from test_HDrezka import TestHDrezka
-from test_film import TestFilms
-from test_cartoon import TestCartoons
-from test_series import TestSeries
-from test_animation import TestAnimations
-from test_announce import TestAnnounce
-from test_new import TestNew
-from test_collections import TestCollections
-from test_search import TestSearch
-from test_meta_data import TestMetaData
 from unittest import main
 
 if __name__ == '__main__':
