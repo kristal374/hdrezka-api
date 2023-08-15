4f6f1852cadd2205efe9f3a2a43eb695a5fea566
kristal <Aleks.golovany@gmail.com>
2023-08-15 15:19:46 +0300

    Изменён импорт на корректный, исправлены тесты

--- tests/test_meta_data.py
+++ tests/test_meta_data.py
@@ -1,18 +1,18 @@
 from unittest import TestCase
 
-import HDrezka
+from HDrezka import __version__
 
 
 class TestMetaData(TestCase):
     def test_metadata_variables(self):
         def is_non_empty_string(s):
             self.assertIsInstance(s, (str, bytes))
-            self.assertEqual(bool(s), True, msg=f"Element is empty")
+            self.assertEqual(bool(s), True, msg="Element is empty")
 
-        is_non_empty_string(HDrezka.__title__)
-        is_non_empty_string(HDrezka.__version__)
-        is_non_empty_string(HDrezka.__license__)
-        is_non_empty_string(HDrezka.__author__)
-        is_non_empty_string(HDrezka.__contact__)
-        is_non_empty_string(HDrezka.__url__)
-        is_non_empty_string(HDrezka.__description__)
+        is_non_empty_string(__version__.__title__)
+        is_non_empty_string(__version__.__version__)
+        is_non_empty_string(__version__.__license__)
+        is_non_empty_string(__version__.__author__)
+        is_non_empty_string(__version__.__contact__)
+        is_non_empty_string(__version__.__url__)
+        is_non_empty_string(__version__.__description__)
