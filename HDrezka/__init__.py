63217eb8a96b073e7a638b5913e921cb88d2dce2
kristal <aleks.golovany@gmail.com>
2023-12-18 01:21:26 +0200

    Убрано бесполезное поле _data

--- HDrezka/__init__.py
+++ HDrezka/__init__.py
@@ -5,51 +5,44 @@ from HDrezka.parse_page import Films, Cartoons, Series, Animation, New, Announce
 from HDrezka.filters import *
 
 __all__ = ["HDrezka", "Films", "Cartoons", "Series", "Animation", "New", "Announce", "Collections", "Search",
-           'GenreFilm', 'GenreCartoons', 'GenreSeries', 'GenreAnimation', 'Filters', 'ShowCategory']
+           'GenreFilm', 'GenreCartoons', 'GenreSeries', 'GenreAnimation', 'Filters', 'ShowCategory', 'NetworkClient']
 
 
 class HDrezka:
     def __init__(self, mirror="https://rezka.ag"):
-        self._data = {"films": Films(),
-                      "cartoons": Cartoons(),
-                      "series": Series(),
-                      "animation": Animation(),
-                      "new": New(),
-                      "announce": Announce(),
-                      "collections": Collections(),
-                      "search": Search()
-                      }
         self.connector = NetworkClient(domain=urlsplit(str(mirror))[1])
 
-    def films(self, genre=None) -> Films:
-        return self._data["films"].selected_category(genre)
+    @staticmethod
+    def films(genre=None) -> Films:
+        return Films().selected_category(genre)
 
-    def cartoons(self, genre=None) -> Cartoons:
-        return self._data["cartoons"].selected_category(genre)
+    @staticmethod
+    def cartoons(genre=None) -> Cartoons:
+        return Cartoons().selected_category(genre)
 
-    def series(self, genre=None) -> Series:
-        return self._data["series"].selected_category(genre)
+    @staticmethod
+    def series(genre=None) -> Series:
+        return Series().selected_category(genre)
 
-    def animation(self, genre=None) -> Animation:
-        return self._data["animation"].selected_category(genre)
+    @staticmethod
+    def animation(genre=None) -> Animation:
+        return Animation().selected_category(genre)
 
-    def new(self) -> New:
-        return self._data["new"]
+    @staticmethod
+    def new() -> New:
+        return New()
 
-    def announce(self) -> Announce:
-        return self._data["announce"]
+    @staticmethod
+    def announce() -> Announce:
+        return Announce()
 
-    def collections(self) -> Collections:
-        return self._data["collections"]
+    @staticmethod
+    def collections() -> Collections:
+        return Collections()
 
-    def search(self, text) -> Search:
-        return self._data["search"].query(text)
+    @staticmethod
+    def search(text) -> Search:
+        return Search().query(text)
 
     def __str__(self):
         return self.connector.url
-
-
-if __name__ == '__main__':
-    rezka = HDrezka()
-    print(rezka.films().selected_category(GenreFilm.COMEDY).filter(Filters.WATCHING))
-    # print(rezka.get("https://rezka.ag/cartoons/fiction/27260-geroi-envella-2017.html").quality("1080p").translate("56").subtitle())
