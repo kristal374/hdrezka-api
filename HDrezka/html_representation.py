8a9703bfccf17c940a29bfae59da842ee717680b
kristal <aleks.golovany@gmail.com>
2024-01-30 13:18:15 +0200

    Добавлена обработка Тегов, для уменьшения лишней работы

--- HDrezka/html_representation.py
+++ HDrezka/html_representation.py
@@ -1,14 +1,14 @@
 from io import IOBase
 from typing import Union, IO
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from requests.models import Response
 
 
 class HTMLDocument:
-    def __init__(self, text: Union[str, bytes, IO, Response]):
+    def __init__(self, text: Union[str, bytes, IO, Response, Tag]):
         self.html = self.__extract_html(text)
-        self.soup = BeautifulSoup(self.html, "lxml")
+        self.soup = BeautifulSoup(self.html, "lxml") if not isinstance(text, Tag) else text
 
     @staticmethod
     def __extract_html(html: Union[str, bytes, IO, Response]) -> str:
@@ -20,9 +20,11 @@ class HTMLDocument:
             return html.decode('utf-8')
         if isinstance(html, str):
             return html
+        if isinstance(html, Tag):
+            return str(html)
         raise AttributeError(f"HTML document cannot be of type {type(html)}")
 
 
 class PageRepresentation:
-    def __init__(self, html_content: Union[str, bytes, IO, Response, HTMLDocument]):
+    def __init__(self, html_content: Union[str, bytes, IO, Response, HTMLDocument, Tag]):
         self.page = html_content if isinstance(html_content, HTMLDocument) else HTMLDocument(html_content)
