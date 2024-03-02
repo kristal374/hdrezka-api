38a7928c28854e862859d37b4c07f2594f7238e3
kristal <aleks.golovany@gmail.com>
2024-03-02 21:50:20 +0200

    Создано представление главной страницы

--- HDrezka/main_page.py
+++ HDrezka/main_page.py
@@ -1,10 +1,17 @@
-from typing import Optional
-from urllib.parse import urlsplit
+from __future__ import annotations
 
+import dataclasses
+import re
+from datetime import datetime
+from typing import Optional, List, TYPE_CHECKING
+from urllib.parse import urlsplit, urljoin
+
+from HDrezka import html_representation, page_representation, movie_page_descriptor, questions_asked, person
 from HDrezka.connector import NetworkClient
 from HDrezka.filters import GenreFilm, GenreSeries, GenreAnimation, GenreCartoons
 from HDrezka.site_navigation import Animation
 from HDrezka.site_navigation import Announce
+from HDrezka.site_navigation import BaseSingleCategory
 from HDrezka.site_navigation import Cartoons
 from HDrezka.site_navigation import Collections
 from HDrezka.site_navigation import Films
@@ -12,12 +19,19 @@ from HDrezka.site_navigation import New
 from HDrezka.site_navigation import QuestionsAsked
 from HDrezka.site_navigation import Search
 from HDrezka.site_navigation import Series
+from HDrezka.utility import convert_string_into_datetime
+from HDrezka.utility import get_url_type, URLsType
+from HDrezka import franchises
 
-__all__ = ["HDrezka"]
+if TYPE_CHECKING:
+    from page_representation import Poster, MovieCollection
+
+__all__ = ["HDrezka", "MainPage"]
 
 
-class HDrezka:
+class HDrezka(BaseSingleCategory):
     def __init__(self, mirror="https://rezka.ag"):
+        super().__init__()
         self.connector = NetworkClient(domain=urlsplit(str(mirror))[1])
 
     @staticmethod
@@ -56,5 +70,127 @@ class HDrezka:
     def questions_asked() -> QuestionsAsked:
         return QuestionsAsked()
 
-    def __str__(self):
-        return self.connector.url
+    def get(self, url: str = None):
+        if url is not None:
+            response = self.connector.get(url).text
+            url_type = get_url_type(url)
+            if url_type == URLsType.main:
+                return MainPageBuilder(response).extract_content()
+            elif url_type == URLsType.movie:
+                return movie_page_descriptor.MovieDetailsBuilder(response).extract_content()
+            elif url_type == URLsType.collections:
+                return page_representation.MovieCollectionBuilder(response).extract_content()
+            elif url_type == URLsType.qa_info:
+                return questions_asked.QuestionsBuilder(response).extract_content()
+            elif url_type == URLsType.qa:
+                return questions_asked.QuestionsBriefInfoBuilder(response).extract_content()
+            elif url_type == URLsType.franchises_info:
+                return franchises.FranchisesBuilder(response).extract_content()
+            elif url_type == URLsType.franchises:
+                return franchises.FranchisesBriefInfoBuilder(response).extract_content()
+            elif url_type == URLsType.person_info:
+                return person.PersonBuilder(response).extract_content()
+            elif url_type == URLsType.poster:
+                return page_representation.PosterBuilder(response).extract_content()
+            else:
+                raise AttributeError("Incorrect url")
+
+        if self.current_page == 1 and not any(self._modifier.values()):
+            return MainPageBuilder(super().get()).extract_content()
+        return page_representation.PosterBuilder(super().get()).extract_content()
+
+
+@dataclasses.dataclass
+class Release:
+    title: str  # Название фильма
+    season: str  # Номер сезона, пример: "1-2 сезон"
+    episode: str  # Номер эпизода, пример: "1-4 серия"/"12 серия"
+    translator: str  # Название озвучки
+    url: str  # ссылка на фильм
+
+    def __repr__(self):
+        return f"<Release({self.title})>"
+
+
+@dataclasses.dataclass
+class DayReleases:
+    date: datetime  # дата в которую были выложены данные релизы
+    releases: List[Release]  # список с релизами этого дня
+
+    def __iter__(self):
+        return iter(self.releases)
+
+    def __repr__(self):
+        return f"DayReleases({self.date})"
+
+
+@dataclasses.dataclass
+class MainPage:
+    best_news: List[Poster] = None  # лучшие новинки за последнее время
+    posters: List[Poster] = None  # список постеров с
+    recommended_collections: List[MovieCollection] = None
+    updates: List[DayReleases] = None
+
+    def __iter__(self):
+        return iter(self.posters)
+
+    def __repr__(self):
+        return f"<MainPage>"
+
+
+class MainPageBuilder(html_representation.PageRepresentation):
+    def extract_content(self):
+        best_news_block = self.page.soup.find("div", id="newest-slider")
+        poster_block = self.page.soup.find("div", class_="b-content__inline_items")
+
+        main_page = MainPage()
+        main_page.best_news = page_representation.PosterBuilder(best_news_block).extract_content()
+        main_page.posters = page_representation.PosterBuilder(poster_block).extract_content()
+        main_page.recommended_collections = self.extract_collections()
+        main_page.updates = self.extract_updates()
+        return main_page
+
+    def __extract_base_url(self):
+        page_url = urlsplit(self.page.soup.find('meta', property='og:url').get("content"))
+        return f"{page_url.scheme}://{page_url.netloc}/"
+
+    def extract_collections(self):
+        result_list = []
+        collections = self.page.soup.find("div", class_="b-collections__newest")
+        base_url = self.__extract_base_url()
+        for collection_obj in collections.find_all("a"):
+            collection = page_representation.MovieCollection()
+            collection.id = int(re.search(r"(?<=collections/)[^\-]\d*", collection_obj.get("href")).group(0))
+            collection.title = collection_obj.span.text.strip()
+            collection.img_url = collection_obj.img.get("src")
+            collection.url = urljoin(base_url, collection_obj.get("href"))
+            result_list.append(collection)
+        return result_list
+
+    def extract_updates(self):
+        result_list = []
+        base_url = self.__extract_base_url()
+        updates_block = self.page.soup.find("div", class_="b-content__inline_sidebar")
+        for day_block in updates_block.find_all("div", class_="b-seriesupdate__block"):
+            release_list = []
+            for item in day_block.find("ul", class_="b-seriesupdate__block_list"):
+                cell = item.find(class_="cell-2")
+                release_list.append(
+                    Release(
+                        title=item.a.string.strip(),
+                        season=item.find(class_="season").string.strip()[1:-1],
+                        episode=cell.next.strip(),
+                        translator=cell.i.string.strip()[1:-1] if cell.i else None,
+                        url=urljoin(base_url, item.a.get("href"))
+                    )
+                )
+            day_obj = day_block.find("div", class_="b-seriesupdate__block_date")
+            date = day_obj.small.string.strip()[1:-1] if day_obj.small else day_obj.next.string.strip()
+            result_list.append(
+                DayReleases(
+                    date=convert_string_into_datetime(date),
+                    releases=release_list
+                )
+            )
+
+        return result_list
