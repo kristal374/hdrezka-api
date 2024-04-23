882242ecb005abd995d45fc274a4d214ca813147
kristal <aleks.golovany@gmail.com>
2024-04-23 14:56:29 +0300

    Изменены некоторые поля для соответствия общему стилю

--- HDrezka/__version__.py
+++ HDrezka/__version__.py
@@ -1,10 +1,10 @@
-__title__ = "HDrezka"
+__title__ = "hdrezka-api"
 __version__ = "1.0.0"
 __license__ = "MIT"
-__author__ = "Alexandr Golovany"
+__author__ = "kristal"
 __contact__ = "mail.aleks.for.work@gmail.com"
 __url__ = "https://github.com/kristal374/hdrezka-api"
-__copyright__ = "Copyright Alexandr Golovany"
+__copyright__ = "Copyright kristal"
 __description__ = (
     "An unofficial Hdrezka.ag API that will help you get" "all the information you need about a video or movie page."
 )
