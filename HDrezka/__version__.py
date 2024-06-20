0c372e1e0cbc7d29e801510e9894063598016cbd
kristal <aleks.golovany@gmail.com>
2024-06-20 12:47:12 +0300

    Исправлена небольшая опечатка

--- HDrezka/__version__.py
+++ HDrezka/__version__.py
@@ -5,5 +5,5 @@ __author__ = "kristal"
 __contact__ = "mail.aleks.for.work@gmail.com"
 __url__ = "https://github.com/kristal374/hdrezka-api"
 __copyright__ = "Copyright kristal"
-__description__ = "An unofficial Hdrezka.ag API that will help you get all " \
+__description__ = "An unofficial HDrezka.ag API that will help you get all " \
                   "the information you need about a video or movie page."
