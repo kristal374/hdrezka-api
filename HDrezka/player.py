c0023183e11b08bb3a6e760b20297265bdcd9e10
kristal <aleks.golovany@gmail.com>
2024-10-18 23:44:57 +0300

    Множественные незначительные улучшения кода

--- HDrezka/player.py
+++ HDrezka/player.py
@@ -4,12 +4,13 @@ import base64
 import json
 import re
 import time
+import warnings
 import zlib
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Optional, Union, List, Dict, Tuple, overload
+from typing import Optional, Union, List, Dict, Tuple, overload, Generic, TypeVar
 
 from bs4 import BeautifulSoup
 
@@ -34,12 +35,18 @@ class Episode:
     id: int
     title: str
 
+    def __repr__(self):
+        return f"<{self.__class__.__name__}({self.title})>"
+
 
 @dataclass
 class Season:
     id: int
     title: str
     episodes: List[Episode] = field(default_factory=list)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}({self.title})>"
 
 
 @dataclass
@@ -77,6 +84,9 @@ class Subtitle:
     code_lang: str
     lang: str
     url: str
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}({self.lang})>"
 
 
 class Actions(str, Enum):
@@ -153,8 +163,11 @@ class SerialQueryData(BaseQueryData):
         }
 
 
-class BaseMovie:
-    _metadata: Union[MovieQueryData, SerialQueryData]
+QueryData = TypeVar("QueryData", bound=BaseQueryData)
+
+
+class BaseMovie(Generic[QueryData]):
+    _metadata: QueryData
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_connector"):
@@ -163,7 +176,7 @@ class BaseMovie:
 
     def __init__(
             self,
-            metadata: BaseQueryData,
+            metadata: QueryData,
             url_dict: Dict[str, List[str]],
             subtitle_list: List[Subtitle],
             translate_list: List[Translator],
@@ -175,6 +188,9 @@ class BaseMovie:
         self._subtitle_list = subtitle_list
         self.translate_list = translate_list
 
+    def get_current_translate(self):
+        return [t for t in self.translate_list if t.id == self._metadata.translator_id][0]
+
     @overload
     def get_video_url(self, quality: None = None) -> Dict[str, List[str]]:
         ...
@@ -183,7 +199,7 @@ class BaseMovie:
     def get_video_url(self, quality: str) -> List[str]:
         ...
 
-    def get_video_url(self, quality: Optional[Quality] = None) -> Union[Dict[str, List[str]], List[str]]:
+    def get_video_url(self, quality: Union[Quality, str, None] = None) -> Union[Dict[str, List[str]], List[str]]:
         if quality is not None and not isinstance(quality, (str, Quality)):
             raise TypeError(
                 f"Attribute 'quality' ({quality}) must be of type 'str' or 'NoneType', "
@@ -199,7 +215,7 @@ class BaseMovie:
                 if url is not None:
                     return url
             raise KeyError("Quality is unavailable.")
-        if self._url_dict.get(quality, False):
+        if not self._url_dict.get(quality, False):
             raise ValueError(
                 f"This 'quality' attribute ({quality}) is not " f"in the quality list {list(self._url_dict.keys())}."
             )
@@ -247,10 +263,10 @@ class BaseMovie:
             )
         return subtitle[0].url
 
-    def set_translate(self, translate: Union[Translator, int, str], is_director: Optional[bool] = False):
+    def _set_translate(self, translate: Union[Translator, int, str], is_director: bool = False):
         if isinstance(translate, Translator):
             if translate in self.translate_list:
-                return self._set_translate(translate)
+                return self._update_translate(translate)
             raise ValueError(
                 f"This Translator ({translate}) is not available for this {self.__class__.__name__.lower()}."
             )
@@ -258,20 +274,33 @@ class BaseMovie:
         if isinstance(translate, int) and not isinstance(translate, bool):
             translates_list = [i for i in self.translate_list if i.id == translate]
             if len(translates_list) == 1:
-                return self._set_translate(translates_list[0])
+                return self._update_translate(translates_list[0])
             if len(translates_list) > 1:
                 # У озвучек фильмов присутствуют поля is_director и т.п. что делает озвучку уникальной,
                 # в то время как ID может повторяться в рамках одного фильма, у сериалов ID всегда уникален.
                 correct_translate = [i for i in translates_list if i.is_director == is_director]
-                return self._set_translate(correct_translate[0])
+                return self._update_translate(correct_translate[0])
             raise ValueError(
                 f"This 'translate' attribute ({translate}) is not in "
                 f"this translator IDs list {[t.id for t in self.translate_list]}."
             )
         if isinstance(translate, str):
-            translates_list = [i for i in self.translate_list if i.title == translate]
-            if len(translates_list) == 1:
-                return self._set_translate(translates_list[0])
+            # Существуют озвучки с одинаковыми названиями, но разными флагами стран.
+            # При сравнении которых title будет идентичен, но по сути это будут
+            # разные озвучки, вследствие чего пользователь мог иметь в виду одну
+            # озвучку, но будет установлена совершенно другая.
+
+            # Я рекомендую для установки желаемой озвучки использовать объект озвучки,
+            # ну или хотя бы ID совместно с is_director(реж. версия) если это необходимо.
+
+            # Тем не менее данный функционал необходим т.к. на сайте все озвучки представлены своими
+            # названиями и пользователю может быть банально удобнее указывать их в таком формате.
+
+            warnings.warn("Using the translates title is less reliable and may result in unexpected behavior.")
+
+            translates_list = [i for i in self.translate_list if translate in (i.title, i.original_title, i.full_title)]
+            if len(translates_list) > 0:
+                return self._update_translate(translates_list[0])
             raise ValueError(
                 f"This 'translate' attribute ({translate}) is not in "
                 f"this translator list {[t.title for t in self.translate_list]}."
@@ -281,7 +310,7 @@ class BaseMovie:
             f"but not of type '{type(translate).__name__}'."
         )
 
-    def _set_translate(self, translate: Translator):
+    def _update_translate(self, translate: Translator):
         self._metadata.translator_id = translate.id
         if isinstance(self, Film):
             self._metadata.is_camrip = translate.is_camrip
@@ -336,35 +365,34 @@ class BaseMovie:
     def load(
             self,
             file_name: str,
-            quality: Quality = Quality.Q1080p,
+            quality: Union[Quality, str] = Quality.Q1080p,
             subtitle: Optional[str] = None,
             create_dump_file: bool = False,
             chunk_size: int = 2 ** 10 * 512,
     ):
-        media_loader.load_from_player(self, f"{file_name}.mp4", quality, create_dump_file, chunk_size)
+        full_path = file_name.format(
+            **{
+                "id": self._metadata.id,
+                "T": self.get_current_translate().title,
+                "t": self._metadata.translator_id,
+                "Q": quality,
+            }
+        )
+        media_loader.load_from_player(self, f"{full_path}.mp4", quality, create_dump_file, chunk_size)
         subtitle_url = self.get_subtitle_url(subtitle) if subtitle is not None else None
         if subtitle_url:
             media_loader.load_from_url(subtitle_url, f"{file_name}.vtt", chunk_size)
 
-
-class Film(BaseMovie):
-    _metadata: MovieQueryData
-
-    def __init__(
-            self,
-            metadata: MovieQueryData,
-            url_dict: Dict[str, List[str]],
-            subtitle_list: List[Subtitle],
-            translate_list: List[Translator],
-    ):
-        super().__init__(metadata, url_dict, subtitle_list, translate_list)
-
     def __repr__(self):
-        return f'<{Film.__name__}(id="{self._metadata.id}")>'
+        return f'<{self.__class__.__name__}(id="{self._metadata.id}")>'
 
 
-class Serial(BaseMovie):
-    _metadata: SerialQueryData
+class Film(BaseMovie[MovieQueryData]):
+    def set_translate(self, translate: Union[Translator, int, str], is_director: bool = False):
+        return self._set_translate(translate=translate, is_director=is_director)
+
+
+class Serial(BaseMovie[SerialQueryData]):
     popularity_translate: Dict[str, float]
 
     def __init__(
@@ -380,6 +408,12 @@ class Serial(BaseMovie):
         self.seasons_tabs = seasons_tabs
         self.popularity_translate = popularity_translate
 
+    def get_current_season(self):
+        return [s for s in self.seasons_tabs if s.id == self._metadata.season][0]
+
+    def get_current_episode(self):
+        return [e for e in self.get_current_season().episodes if e.id == self._metadata.episode][0]
+
     @overload
     def set_season(self, season_id: int) -> Serial:
         ...
@@ -388,7 +422,7 @@ class Serial(BaseMovie):
     def set_season(self, *, season_title: str) -> Serial:
         ...
 
-    def set_season(self, season_id: Optional[int] = None, *, season_title: Optional[str] = None) -> Serial:
+    def set_season(self, season_id: Optional[int] = None, *, season_title: Optional[str] = None):
         if season_id is not None and season_title is not None:
             raise ValueError("Expected either 'season_id' or 'season_title', but not both.")
         if season_id is None and season_title is None:
@@ -428,7 +462,7 @@ class Serial(BaseMovie):
     def set_episode(self, *, episode_title: str) -> Serial:
         ...
 
-    def set_episode(self, episode_id: Optional[int], *, episode_title: Optional[str] = None):
+    def set_episode(self, episode_id: Optional[int] = None, *, episode_title: Optional[str] = None):
         if episode_id is not None and episode_title is not None:
             raise ValueError("Expected either 'episode_id' or 'episode_title', but not both.")
         if episode_id is None and episode_title is None:
@@ -443,18 +477,18 @@ class Serial(BaseMovie):
                 f"Attribute 'episode_title' ({episode_title}) must be of type 'str', "
                 f"but not of type '{type(episode_title).__name__}'."
             )
-        target_season = [s for s in self.seasons_tabs if s.id == self._metadata.season]
-        target_episode = [e for e in target_season[0].episodes if e.id == episode_id or e.title == episode_title]
+        target_season = self.get_current_season()
+        target_episode = [e for e in target_season.episodes if e.id == episode_id or e.title == episode_title]
 
         if len(target_episode) == 0:
             if episode_title is not None:
                 raise ValueError(
                     f"This 'episode_title' attribute ({episode_title}) is missing "
-                    f"from episode list {[e.title for e in target_season[0].episodes]}"
+                    f"from episode list {[e.title for e in target_season.episodes]}"
                 )
             raise ValueError(
                 f"This 'episode_id' attribute ({episode_id}) is missing "
-                f"from episode IDs list {[e.id for e in target_season[0].episodes]}"
+                f"from episode IDs list {[e.id for e in target_season.episodes]}"
             )
         self._metadata.episode = episode_id
         self._update_state()
@@ -465,7 +499,7 @@ class Serial(BaseMovie):
             # При смене озвучки у сериала мы должны дополнительно запросить список серий и эпизодов
             # поскольку у разных озвучек он может отличаться, для этого при запросе надо сменить action
             self._metadata.action = Actions.get_episodes
-            return super().set_translate(translate=translate, is_director=None)
+            return self._set_translate(translate=translate)
         finally:
             self._metadata.action = Actions.get_stream
 
@@ -476,8 +510,7 @@ class Serial(BaseMovie):
             season_title: Optional[str] = None,
             episode_id: Optional[int] = None,
             episode_title: Optional[str] = None,
-            translate: Union[Translator, int, None] = None,
-            is_director: bool = False,
+            translate: Union[Translator, int, str, None] = None,
     ):
         if season_id is not None and season_title is not None:
             raise ValueError("Expected either 'season_id' or 'season_title', but not both.")
@@ -489,7 +522,7 @@ class Serial(BaseMovie):
         # Это необходимо поскольку у новой озвучки может быть озвучено другое
         # количество серий и сезонов.
         if translate is not None:
-            self.set_translate(translate=translate, is_director=is_director)
+            self.set_translate(translate=translate)
 
         # Для уменьшения количества запросов блокируем обновление объекта
         with self._block_updates():
@@ -517,7 +550,15 @@ class Serial(BaseMovie):
     ) -> List[Season]:
         # season_to = -1 До конца сериала
         # season_to = -2 До конца сезона
-        # TODO
+        if not isinstance(season_from, int):
+            raise TypeError(f"'season_from' should be of type 'int', but got {type(season_from).__name__}.")
+        if not isinstance(season_to, int):
+            raise TypeError(f"'season_to' should be of type 'int', but got {type(season_to).__name__}.")
+        if not isinstance(episode_from, int):
+            raise TypeError(f"'episode_from' should be of type 'int', but got {type(episode_from).__name__}.")
+        if not isinstance(episode_to, int):
+            raise TypeError(f"'episode_to' should be of type 'int', but got {type(episode_to).__name__}.")
+
         if season_to == -1:
             season_to = self.seasons_tabs[-1].id
             episode_to = self.seasons_tabs[-1].episodes[-1].id
@@ -525,20 +566,53 @@ class Serial(BaseMovie):
             season_to = season_from
             episode_to = [s for s in self.seasons_tabs if s.id == season_from][0].episodes[-1].id
 
-        result_list = []
+        min_season = self.seasons_tabs[0].id
+        max_season = self.seasons_tabs[-1].id
+        min_episode_start = self.seasons_tabs[0].episodes[0].id
+        max_episode_start = self.seasons_tabs[0].episodes[-1].id
+        min_episode_end = self.seasons_tabs[-1].episodes[0].id
+        max_episode_end = self.seasons_tabs[-1].episodes[-1].id
 
+        if not min_season <= season_from <= max_season:
+            raise ValueError(
+                f"The start season number '{season_from}' must be " f"between '{min_season}' and '{max_season}'."
+            )
+        if not min_season <= season_to <= max_season:
+            raise ValueError(
+                f"The end season number '{season_to}' must be " f"between '{min_season}' and '{max_season}'."
+            )
+        if season_from > season_to:
+            raise ValueError(
+                f"The start season number '{season_from}' cannot be "
+                f"greater than the end season number '{season_to}'."
+            )
+
+        if season_from == season_to and episode_from > episode_to:
+            raise ValueError(
+                f"The start episode number '{episode_from}' cannot be "
+                f"greater than the end episode number '{episode_to}'."
+            )
+        if not min_episode_start <= episode_from <= max_episode_start:
+            raise ValueError(
+                f"The start episode number '{episode_from}' must be "
+                f"between '{min_episode_start}' and '{max_episode_start}' in this season."
+            )
+        if not min_episode_end <= episode_to <= max_episode_end:
+            raise ValueError(
+                f"The end episode number '{episode_to}' must be "
+                f"between '{min_episode_end}' and '{max_episode_end}' in this season."
+            )
+
+        result_list = []
         for season in self.seasons_tabs:
             if season.id < season_from or season.id > season_to:
                 continue
 
             sliced_episodes = season.episodes.copy()
-
             if season.id == season_from:
                 sliced_episodes = [e for e in sliced_episodes if e.id >= episode_from]
-
             if season.id == season_to:
                 sliced_episodes = [e for e in sliced_episodes if e.id <= episode_to]
-
             result_list.append(Season(season.id, season.title, sliced_episodes))
 
         return result_list
@@ -550,7 +624,7 @@ class Serial(BaseMovie):
             episode_start: int = 1,
             season_end: int = -1,
             episode_end: int = 1,
-            quality: Quality = Quality.Q1080p,
+            quality: Union[Quality, str] = Quality.Q1080p,
             subtitle: Optional[str] = None,
             create_dump_file: bool = False,
             chunk_size: int = 2 ** 10 * 512,
@@ -569,7 +643,7 @@ class Serial(BaseMovie):
                         "s": season.id,
                         "E": episode.title,
                         "e": episode.id,
-                        "T": [t.title for t in self.translate_list if t.id == self._metadata.translator_id][0],
+                        "T": self.get_current_translate().title,
                         "t": self._metadata.translator_id,
                         "Q": quality,
                     }
@@ -603,9 +677,6 @@ class Serial(BaseMovie):
             self._metadata.season = self.seasons_tabs[0].id
             self._metadata.episode = self.seasons_tabs[0].episodes[0].id
         return response
-
-    def __repr__(self):
-        return f'<{Serial.__name__}(id="{self._metadata.id}")>'
 
 
 class PlayerBuilder(PageRepresentation):
@@ -815,7 +886,7 @@ class PlayerBuilder(PageRepresentation):
         return seasons_list
 
     @staticmethod
-    def make_seasons_list(seasons_string: str):
+    def make_seasons_list(seasons_string: str) -> List[Season]:
         soup = BeautifulSoup(seasons_string, "lxml")
         seasons_list = []
         for s in soup.find_all(class_="b-simple_season__item"):
@@ -838,3 +909,6 @@ class PlayerBuilder(PageRepresentation):
                     )
                 )
         return episodes_lists
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}>"
